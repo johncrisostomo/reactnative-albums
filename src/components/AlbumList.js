3eaa5152a5975fb247ee8e748c334a0b6835089a
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-17 15:40:15 +0800

    adds axios to project and implements get

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: [],
+    };
+  }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => this.setState({ albums: response.data }) )
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>{JSON.stringify(this.state.albums)}</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
