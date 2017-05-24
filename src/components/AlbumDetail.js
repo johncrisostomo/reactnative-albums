62fbebb2efe25a2beee2f59f5da40429a5f49e7a
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-24 08:02:00 +0800

    displays album image in a separate section

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,29 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailContainerStyle,
+    thumbnailStyle,
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image style={styles.thumbnailStyle}
-            source={ { uri: thumbnail_image } } />
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle}
+            source={{ uri: thumbnail_image }} />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -26,10 +37,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     width: 50,
     height: 50,
   },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  }
 };
 
 export default AlbumDetail;
